f8e707115206a77e9c6f0c3b8b62c7316abc8874
Yuchen Zhang <yuchenzhang@me.com>
2012-11-19 22:46:43 +0100

    check out rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pitchify::Application.config.secret_token = '7b8a2d161e278ff4790d743be48b177a2af402d10d711fe580da3863006e52f1b6d4c02a7882b6deda423953e1ae6a317935f27349f202613d1f80e976430bb7'
